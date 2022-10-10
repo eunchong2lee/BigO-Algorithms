5da3a7a64d66fdcf5c8b1923df7ce68fa2829a0e
eunchong2lee <tlfqjrjs1234@naver.com>
2022-10-10 21:48:24 +0900

    [Complete] Study

--- 8_DataStructres/2_DS.js
+++ 8_DataStructres/2_DS.js
@@ -90,6 +90,96 @@ class SinglyLinkedList {
         this.length++;
         return this;
     }
+
+    // ****************************************************************
+    // get
+
+    get(index) {
+        if(index < 0 || index >=this.length) return null;
+        let counter = 0;
+        let current = this.head;
+        while(counter != index) {
+            current = current.next;
+            counter ++;
+        }
+        return current;
+    }
+    
+    // ****************************************************************
+    // set
+
+    set(index, val) {
+        let foundNode = this.get(index);
+        if(foundNode) {
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
+
+    // ****************************************************************
+    // insert
+
+    insert(index, val) {
+        if( index < 0 || index > this.length) return false;
+        if(index === this.length) return !!this.push(val);
+        if(index === 0) return !!this.unshift(val);
+
+        let newNode = new Node(val);        
+        let prev = this.get(index - 1);
+        let temp = prev.next;
+        prev.next= newNode;
+        newNode.next = temp;
+        this.length++;
+        return true;
+    }
+
+    // ****************************************************************
+    // insert
+
+    remove(index) {
+        if(index < 0 || index >= this.length) return undefined;
+        if(index === 0) return this.shift();
+        if(index === this.length) return this.pop();
+        
+        let previousNode = this.get(index - 1);
+        let removed = previousNode.next();
+        previousNode.next = removed.next();
+        this.length--;
+
+        return removed;
+    }
+
+    // ****************************************************************
+    // reverse
+
+    reverse() {
+        let node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+        let next;
+        let prev = null;
+
+        for(let i = 0; i < this.length; i++) {
+            next = node.next();
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+        return this;
+    }
+
+    // ****************************************************************
+    // print()
+    print() {
+        let arr = [];
+        let current = this.head;
+        while(current){
+            arr.push(current.val)
+            current = current.next;
+        }
+        console.log(arr);
+    }
 }
 let list= new SinglyLinkedList();
 console.log(list)
@@ -101,6 +191,15 @@ list.push("my name is Eunchong");
 console.log(list);
 list.unshift("hellohello");
 console.log(list);
+list.insert(1, "insert");
+console.log(list);
 
+
+// ********************************
+// BigO
+// insertion O(1)
+// removal O(1) or O(N)
+// searching O(N)
+// Access O(N)
 
 
